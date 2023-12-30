7849d6a49013c8d9024619b400a15e7477bec37d
aztecia <142163575+aztecia@users.noreply.github.com>
2023-12-30 04:35:12 -0500

    Update unethical bot.lua

--- unethical bot.lua
+++ unethical bot.lua
@@ -5,7 +5,68 @@ Players, RunService, HttpService, TPService = game:GetService("Players"), game:G
 
 LocalPlayer, PlaceID = Players.LocalPlayer, game.PlaceId
 
+local function Chat(msg) game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(msg, "All") end
 
+game:GetService("RunService"):Set3dRenderingEnabled(false)
+local a = game
+local b = a.Workspace
+local c = a.Lighting
+local d = b.Terrain
+d.WaterWaveSize = 0
+d.WaterWaveSpeed = 0
+d.WaterReflectance = 0
+d.WaterTransparency = 0
+c.GlobalShadows = false
+c.FogEnd = 9e9
+c.Brightness = 0
+settings().Rendering.QualityLevel = "Level01"
+for e, f in pairs(a:GetDescendants()) do
+   if f:IsA("Part") or f:IsA("Union") or f:IsA("CornerWedgePart") or f:IsA("TrussPart") then
+       f.Material = "Plastic"
+       f.Reflectance = 0
+   elseif f:IsA("Decal") or f:IsA("Texture") then
+       f.Transparency = 0
+   elseif f:IsA("ParticleEmitter") or f:IsA("Trail") then
+       f.Lifetime = NumberRange.new(0)
+   elseif f:IsA("Explosion") then
+       f.BlastPressure = 0
+       f.BlastRadius = 0
+   elseif f:IsA("Fire") or f:IsA("SpotLight") or f:IsA("Smoke") or f:IsA("Sparkles") then
+       f.Enabled = false
+   elseif f:IsA("MeshPart") then
+       f.Material = "Plastic"
+       f.Reflectance = 0
+       f.TextureID = 10385902758728957
+   end
+end
+for e, g in pairs(c:GetChildren()) do
+   if
+       g:IsA("BlurEffect") or g:IsA("SunRaysEffect") or g:IsA("ColorCorrectionEffect") or g:IsA("BloomEffect") or
+           g:IsA("DepthOfFieldEffect")
+    then
+       g.Enabled = false
+   end
+end
+sethiddenproperty(game.Lighting, "Technology", "Compatibility")
+wait(1)
+for i,v in next, workspace:GetDescendants() do
+if v:IsA("MeshPart") or v:IsA("UnionOperation") then
+sethiddenproperty(v, "RenderFidelity", "Automatic")
+end
+end
+wait(1)
+local timeBegan = tick()
+for i,v in ipairs(workspace:GetDescendants()) do
+if v:IsA("BasePart") then
+v.Material = "SmoothPlastic"
+end
+end
+for i,v in ipairs(game:GetService("Lighting"):GetChildren()) do
+v:Destroy()
+end
+local timeEnd = tick() - timeBegan
+local timeMS = math.floor(timeEnd*1000)
+print("SmoothFPS loaded successfully in " .. timeMS .. "ms")
 
 --[ Tables ]--
 local ChatLists = {
@@ -166,7 +227,24 @@ end
 --------------------------------------------------------------------------
 
 --[ Table Checks ]--
+local CheckUser = function(User)
+    local UserId = User["UserId"]
 
+    if table.find(kroneUserids, UserId) then
+--        table.insert(WhitelistedPlayers, UserId)
+        Chat("Presence of the Owner \"" .. User["DisplayName"] .. "\" has been detected.")
+    end
+end
+
+for _,v in pairs(game.Players:GetPlayers()) do
+    if v ~= game.Players.LocalPlayer then
+        CheckUser(v)
+    end
+end
+
+game.Players["PlayerAdded"]:Connect(function(v)
+    CheckUser(v)
+end)
 --------------------------------------------------------------------------
 
 --------------------------------------------------------------------------
@@ -234,9 +312,7 @@ end
 --------------------------------------------------------------------------
 
 --[ Chat System ]--
-local function Chat(msg)
-    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(msg, "All")
-end
+
 
 task.spawn(function() 
     while true do 
