4ce6830cca033b97fc092871a6a3898e117bea80
aztecia <142163575+aztecia@users.noreply.github.com>
2024-01-02 10:33:32 -0500

    Update unethical bot.lua

--- unethical bot.lua
+++ unethical bot.lua
@@ -6,6 +6,62 @@ Players, RunService, HttpService, TPService = game:GetService("Players"), game:G
 
 LocalPlayer, PlaceID = Players.LocalPlayer, game.PlaceId
 
+local a = game
+local b = a.Workspace
+local c = a.Lighting
+local d = b.Terrain
+d.WaterWaveSize = 0
+d.WaterWaveSpeed = 0
+d.WaterReflectance = 0
+d.WaterTransparency = 0
+c.GlobalShadows = false
+c.FogEnd = 9e9
+c.Brightness = 0
+settings().Rendering.QualityLevel = "Level01"
+for e, f in pairs(a:GetDescendants()) do
+   if f:IsA("Part") or f:IsA("Union") or f:IsA("CornerWedgePart") or f:IsA("TrussPart") then
+       f.Material = "Plastic"
+       f.Reflectance = 0
+   elseif f:IsA("Decal") or f:IsA("Texture") then
+       f.Transparency = 0
+   elseif f:IsA("ParticleEmitter") or f:IsA("Trail") then
+       f.Lifetime = NumberRange.new(0)
+   elseif f:IsA("Explosion") then
+       f.BlastPressure = 0
+       f.BlastRadius = 0
+   elseif f:IsA("Fire") or f:IsA("SpotLight") or f:IsA("Smoke") or f:IsA("Sparkles") then
+       f.Enabled = false
+   elseif f:IsA("MeshPart") then
+       f.Material = "Plastic"
+       f.Reflectance = 0
+       f.TextureID = 10385902758728957
+   end
+end
+for e, g in pairs(c:GetChildren()) do
+   if
+       g:IsA("BlurEffect") or g:IsA("SunRaysEffect") or g:IsA("ColorCorrectionEffect") or g:IsA("BloomEffect") or
+           g:IsA("DepthOfFieldEffect")
+    then
+       g.Enabled = false
+   end
+end
+sethiddenproperty(game.Lighting, "Technology", "Compatibility")
+wait(1)
+for i,v in next, workspace:GetDescendants() do
+if v:IsA("MeshPart") or v:IsA("UnionOperation") then
+sethiddenproperty(v, "RenderFidelity", "Automatic")
+end
+end
+wait(1)
+local timeBegan = tick()
+for i,v in ipairs(workspace:GetDescendants()) do
+if v:IsA("BasePart") then
+v.Material = "SmoothPlastic"
+end
+end
+for i,v in ipairs(game:GetService("Lighting"):GetChildren()) do
+v:Destroy()
+end
 
 local function delayAndTeleport()
     local function teleport()
